a9dcc1fd6a8dc1f80d9c138d8ccc0214c0c33c61
Franck <50842288+Enhax@users.noreply.github.com>
2024-09-02 17:51:10 +0200

    slider

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+const slides = document.querySelectorAll('.header-img');
+const prevSlideButton = document.querySelector('.control-prev');
+const nextSlideButton = document.querySelector('.control-next');
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener('DOMContentLoaded', initializeSlider)
+
+function initializeSlider() {
+    if (slides.length > 0) {
+        slides[slideIndex].classList.add('displaySlide');
+        intervalId = setInterval(nextSlide, 5000);
+    }
+}
+
+function showSlide(index) {
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    } else {
+        slideIndex = index;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove('displaySlide');
+    });
+    slides[slideIndex].classList.add('displaySlide');
+}
+
+function nextSlide() {
+    slideIndex++
+    showSlide(slideIndex);
+}
+
+prevSlideButton.addEventListener('click', () => {
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+    intervalId = setInterval(nextSlide, 5000);
+});
+
+nextSlideButton.addEventListener('click', () => {
+    clearInterval(intervalId);
+    slideIndex++;
+    showSlide(slideIndex);
+    intervalId = setInterval(nextSlide, 5000);
+});
